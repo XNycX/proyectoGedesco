b753977a3766c5aa0e21e0f672e4dcdd4d9cee61
XNycX <nycx2009@hotmail.com>
2022-04-21 14:15:11 +0200

    redux, reducer, type, store & action product implemented

--- src/redux/actions/product.js
+++ src/redux/actions/product.js
@@ -1,2 +1,17 @@
 import store from "../store";
 import axios from "axios";
+import { GET_PRODUCTS } from "../types";
+const API_URL = 'https://fakestoreapi.com';
+
+export const getProcuts = async () => {
+    try {
+        const res = await axios.get(`${API_URL}/products`);
+        store.dispatch({
+            type: GET_PRODUCTS,
+            payload: res.data,
+        });
+        
+    } catch (error) {
+        console.error(error)
+    }
+}
