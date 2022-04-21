3843a4ba183cc15fbe2e4b8578baa2aeacca419f
XNycX <nycx2009@hotmail.com>
2022-04-21 15:54:14 +0200

    ant design installed

--- src/Components/Products/Product.jsx
+++ src/Components/Products/Product.jsx
@@ -1,15 +1,20 @@
 import React, { useEffect } from "react";
+import { connect } from 'react-redux';
 import "./Product.css";
 import { getProducts } from "../../redux/actions/product";
 
-const Product = () => {
+const Product = (props) => {
     useEffect(() => {
         getProducts();
     }, [])
 
   return (
-    <div>Product</div>
+      <div>Products{console.log(props.products) }</div>
   )
 }
 
-export default (Product)
+const mapStateToProps = (state) => ({
+    products: state.products.products
+});
+
+export default  connect(mapStateToProps)(Product)
