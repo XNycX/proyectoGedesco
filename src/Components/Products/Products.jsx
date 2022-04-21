8a0eff71ad7e117c7445fe13b4346f1bab3d96ef
XNycX <nycx2009@hotmail.com>
2022-04-21 18:17:45 +0200

    redux connected to the component Products

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { getProducts } from "../../redux/actions/product";
+import { connect } from "react-redux";
 
 const Products = () => {
+    useEffect(() => {
+        getProducts();
+      }, []);
   return (
     <div>Products</div>
   )
 }
+const mapStateToProps = (state) => ({
+    products: state.products.products
+  });
 
-export default Products
+export default connect(mapStateToProps)(Products)
