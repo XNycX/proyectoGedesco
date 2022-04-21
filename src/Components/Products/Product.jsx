477bf1bd5eea993f9ef96c64680286cc58a0a703
XNycX <nycx2009@hotmail.com>
2022-04-21 16:36:30 +0200

    Table to show products added

--- src/Components/Products/Product.jsx
+++ src/Components/Products/Product.jsx
@@ -2,14 +2,37 @@ import React, { useEffect } from "react";
 import { connect } from 'react-redux';
 import "./Product.css";
 import { getProducts } from "../../redux/actions/product";
+import { Table } from 'antd';
+import "antd/dist/antd.css";
+
 
 const Product = (props) => {
+    const columns = [
+        {
+          title: 'Title',
+          dataIndex: 'title',
+          width: '30%',
+        },
+        {
+          title: 'Price',
+          dataIndex: 'price',
+          sorter: (a, b) => a.price - b.price,
+        },
+        {
+          title: 'Categories',
+          dataIndex: 'category',
+          width: '40%',
+        },
+    ];
+    
     useEffect(() => {
         getProducts();
     }, [])
-
+    
   return (
-      <div>Products{console.log(props.products) }</div>
+      <div>
+          <Table columns={columns} dataSource={props.products} />
+      </div>
   )
 }
 
