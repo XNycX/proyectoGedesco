ef138c9e4d3803b1485403385e6c4d07ce33e678
XNycX <nycx2009@hotmail.com>
2022-04-21 13:33:56 +0200

    Components folder created with the component Product created

---
+++ src/Components/Products/Product.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import "./Product.css";
+
+const Product = () => {
+  return (
+    <div>Product</div>
+  )
+}
+
+export default Product
